d0375ab175c41878a64edafad014d2572d475e1e
ganeshbelote <ganeshbelote18@gmail.com>
2025-07-27 11:11:16 +0530

    fixed : route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,8 +10,7 @@ function App() {
     <Router>
       <ToastContainer position='top-right' autoClose={3000} />
       <Routes>
-        <Route path='/' element={<Navigate to='/login' />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/home' element={<Home />} />
         <Route path='*' element={<NotFound />} />
