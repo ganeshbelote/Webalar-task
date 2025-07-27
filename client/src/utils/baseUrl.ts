085cbdcf3a6e804be591d1c8271691b8f6b41f2e
ganeshbelote <ganeshbelote18@gmail.com>
2025-07-27 10:22:31 +0530

    changed : changed bas url

--- client/src/utils/baseUrl.ts
+++ client/src/utils/baseUrl.ts
@@ -1,2 +1,2 @@
-const baseUrl = "http://localhost:5000" //https://webalar-task.onrender.com & http://localhost:5000
+const baseUrl = "https://webalar-task.onrender.com" //https://webalar-task.onrender.com & http://localhost:5000
 export default baseUrl
